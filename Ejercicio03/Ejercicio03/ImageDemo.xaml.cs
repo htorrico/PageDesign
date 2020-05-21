11e0e4a4ffe4502964b0ba1de64fa0a6b0227ce4
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-21 18:49:39 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio03/Ejercicio03/ImageDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImageDemo : ContentPage
+	{
+		public ImageDemo ()
+		{
+			InitializeComponent ();
+            //var image = new Image { Source = "logotest.jpg" };
+            //StackLayout stack = new StackLayout();
+            //stack.Children.Add(image);
+            //Content = stack;
+
+        }
+	}
+}
---
+++ Ejercicio03/Ejercicio03/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ButtonDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ButtonCode());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DatepickerDemo());
+
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EditorDemo());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EditorCode());
+
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EntryDemo());
+
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EntryCode());
+
+            };
+            Item8.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LabelDemo());
+
+            };
+            Item9.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LabelCode());
+
+            };
+            Item10.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LocalImage());
+
+            };
+            Item11.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EmbeddedImage());
+
+            };
+            Item12.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+
+            };
+            Item13.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewCode());
+
+            };
+            Item14.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewSource());
+
+            };
+        }
+    }
+}
